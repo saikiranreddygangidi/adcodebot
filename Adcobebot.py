3017c3becd209285c6b14ab4235cd5f066c1c72f
saikiranreddygangidi <40819884+saikiranreddygangidi@users.noreply.github.com>
2020-06-24 22:42:33 +0530

    u

--- Adcobebot.py
+++ Adcobebot.py
@@ -159,7 +159,7 @@ def codename(message):
     elif value.text=='N' or value.text=='n':
       bot.reply_to(message,'select one program  language among the  following \n /c\n/cpp\n/java\n/python')
       break
-    else
+    else:
       bot.reply_to(message,value)
 
   # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
